4802e4bbcc60db8e8bb2afd8d00b80785f3929bf
kraman14 <kumar.raman@yahoo.com>
2016-11-20 21:15:03 -0800

    Trying auto deploy 5.

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_08cae0b40b89_key -iv $encrypted_08cae0b40b89_iv -in codesigning.asc.enc -out signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_08cae0b40b89_key -iv $encrypted_08cae0b40b89_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
 fi
